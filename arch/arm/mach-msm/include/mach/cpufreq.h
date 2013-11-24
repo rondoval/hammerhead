b81ff24231027e2bcab8a120a338e210c70cf6dc
Saravana Kannan <skannan@codeaurora.org>
2013-11-24 03:45:00 -0800

    devfreq: Add MSM CPUfreq governor
    The MSM CPUfreq devfreq governor determines the CPU to DDR bandwidth vote
    based on the current CPU frequency of all the active CPUs.
    
    This functionality used to be a part of the MSM CPUfreq driver that
    directly voted for the CPU to DDR bandwidth using MSM bus scaling APIs.
    This refactor decouples CPU to DDR BW scaling from CPU frequency and allows
    switch between various CPU BW governors.
    
    The bandwidth values in the msm-cpufreq table have to be updated since the
    new CPU BW driver does the MBps to Bps conversion correctly. The MSM
    CPUfreq driver used to do * 1000 * 1000 to convert from MBps to Bps instead
    of doing * 1024 * 1024.
    
    Change-Id: I184f09d628a1b27d52506d2f760d65831f1a1110
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    (cherry picked from commit 4bbc993b178bca1969f2e4b122a32e0e589ee839)
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/include/mach/cpufreq.h
+++ arch/arm/mach-msm/include/mach/cpufreq.h
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 /* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+=======
+/*
+ * Copyright (c) 2013 The Linux Foundation. All rights reserved.
+>>>>>>> f98e838... devfreq: Add MSM CPUfreq governor
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -9,6 +14,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+<<<<<<< HEAD
 
 #ifndef __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
 #define __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
@@ -43,3 +49,32 @@ static inline int msm_cpufreq_set_freq_limits(
 #endif
 
 #endif /* __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H */
+=======
+#ifndef __MACH_CPUFREQ_H
+#define __MACH_CPUFREQ_H
+
+#if defined(CONFIG_DEVFREQ_GOV_MSM_CPUFREQ)
+extern int devfreq_msm_cpufreq_update_bw(void);
+extern int register_devfreq_msm_cpufreq(void);
+#else
+static int devfreq_msm_cpufreq_update_bw(void)
+{
+	return 0;
+}
+static int register_devfreq_msm_cpufreq(void)
+{
+	return 0;
+}
+#endif
+
+#if defined(CONFIG_CPU_FREQ_MSM)
+extern unsigned long msm_cpufreq_get_bw(void);
+#else
+extern unsigned long msm_cpufreq_get_bw(void)
+{
+	return ULONG_MAX;
+}
+#endif
+
+#endif
+>>>>>>> f98e838... devfreq: Add MSM CPUfreq governor
