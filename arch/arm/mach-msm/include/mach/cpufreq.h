432d6dcaf1d718496eac00a8a5bb8651e17afb9b
franciscofranco <franciscofranco.1990@gmail.com>
2014-01-14 23:46:43 +0000

    msm: cpufreq: fix merge conflict
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/include/mach/cpufreq.h
+++ arch/arm/mach-msm/include/mach/cpufreq.h
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
-=======
 /*
  * Copyright (c) 2013 The Linux Foundation. All rights reserved.
->>>>>>> f98e838... devfreq: Add MSM CPUfreq governor
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -14,42 +10,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-<<<<<<< HEAD
-
-#ifndef __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
-#define __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H
-
-#define MSM_CPUFREQ_NO_LIMIT 0xFFFFFFFF
-
-#ifdef CONFIG_CPU_FREQ_MSM
-
-/**
- * msm_cpufreq_set_freq_limit() - Set max/min freq limits on cpu
- *
- * @cpu: The cpu core for which the limits apply
- * @max: The max frequency allowed
- * @min: The min frequency allowed
- *
- * If the @max or @min is set to MSM_CPUFREQ_NO_LIMIT, the limit
- * will default to the CPUFreq limit.
- *
- * returns 0 on success, errno on failure
- */
-extern int msm_cpufreq_set_freq_limits(
-		uint32_t cpu, uint32_t min, uint32_t max);
-
-extern void msm_cpufreq_set_min_freq_limit(
-		uint32_t cpu, uint32_t min);
-#else
-static inline int msm_cpufreq_set_freq_limits(
-		uint32_t cpu, uint32_t min, uint32_t max)
-{
-	return -ENOSYS;
-}
-#endif
-
-#endif /* __ARCH_ARM_MACH_MSM_MACH_CPUFREQ_H */
-=======
 #ifndef __MACH_CPUFREQ_H
 #define __MACH_CPUFREQ_H
 
@@ -77,4 +37,3 @@ extern unsigned long msm_cpufreq_get_bw(void)
 #endif
 
 #endif
->>>>>>> f98e838... devfreq: Add MSM CPUfreq governor
